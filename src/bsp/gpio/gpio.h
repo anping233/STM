9f79ad7624f657a9e135708b6a57505ec8566599
anping233 <112800107+anping233@users.noreply.github.com>
2023-07-10 15:19:45 +0800

    1.添加led和usart的gpio引脚信息

---
+++ src/bsp/gpio/gpio.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define LED0_GPIO_PORT      (GPIOC)
+#define LED0_GPIO_PIN       (GPIO_Pin_13)
+
+
+#define USART1_GPIO_PORT        (GPIOA)
+#define USART1_GPIO_TXD_PIN     (GPIO_Pin_9)
+#define USART1_GPIO_RXD_PIN     (GPIO_Pin_10)
+
+
