9f8fdee9085f92e07493e961547732fd98c4017c
anping233 <112800107+anping233@users.noreply.github.com>
2023-07-22 09:35:43 +0800

    修改错误，函数参数与函数体中的不匹配

--- src/driver/delay/delay.c
+++ src/driver/delay/delay.c
@@ -1,11 +1,26 @@
 #include "include.h"
 void delay_ms(size_t ms)
-{
+{// TODO: 将该函数改为基于systick实现并放入systick.c中
     for(int i = 0; i < 1000; i++) {
-        __asm
-        (
-            "NOP"
-        );
+        for (size_t j = 0; j < ms; j++) {
+            __asm
+            (
+                "NOP"
+            );
+        }
     }
+    return;
+}
 
+void delay_us(size_t us)
+{// TODO: 将该函数改为基于systick实现并放入systick.c中
+    for(int i = 0; i < 10; i++) {
+        for (size_t j = 0; j < us; j++) {
+            __asm
+            (
+                "NOP"
+            );
+        }
+    }
+    return;
 }
--- src/driver/delay/delay.h
+++ src/driver/delay/delay.h
@@ -1,6 +1,7 @@
 #pragma once
 
 void delay_ms(size_t ms);
+void delay_us(size_t ms);
 
 
 
