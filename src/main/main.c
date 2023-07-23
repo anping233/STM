bb6eba00cd5c57f910f020a61a050caf4eb00010
anping233 <112800107+anping233@users.noreply.github.com>
2023-07-23 11:17:08 +0800

    添加key,oled,led1的测试函数

--- src/main/main.c
+++ src/main/main.c
@@ -10,6 +10,10 @@ int main(int argc, char **argv) {
             led_test();
          #endif
 
+         #if LED1_TEST
+            led1_test();
+         #endif
+
          #if USART_TEST
             usart_test();
          #endif
@@ -23,10 +27,18 @@ int main(int argc, char **argv) {
          #endif
 
          #if TEST_24C256
-            uint8_t temp = 0;
-            temp = test_24c256();
-            printf("%d",temp);
+            uint8_t temp1 = 0;
+            temp1 = test_24c256();
+            printf("%d",temp1);
 
+         #endif
+
+         #if OLED_TEST
+         oled_test();
+         #endif
+
+         #if KEY_TEST
+         key_test();
          #endif
 	 }
     return 0;
