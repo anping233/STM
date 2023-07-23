70891ea2ecf4041dc980d2a460e25ee3400efdee
anping233 <112800107+anping233@users.noreply.github.com>
2023-07-23 11:13:24 +0800

    添加key模块

---
+++ src/driver/key/key.c
@@ -0,0 +1,50 @@
+#include "include.h"
+
+void key_init(void)
+{
+    key_gpio_init();
+
+    return;
+}
+
+void push_key1(uint8_t mode)
+{
+    static uint8_t key_up = 1;
+
+    if(mode == 1)
+    key_up = 1;
+
+    if(key_up && gpio_get(GPIOB, gpio_pin1))
+    {
+        key_up = 0;
+        delay_ms(10);
+        led1_toggle();
+    }
+    return;
+}
+
+void push_key2(uint8_t mode)
+{
+    static uint8_t key_up = 1;
+
+    if(mode == 1)
+    key_up = 1;
+
+    if(key_up && gpio_get(GPIOB, gpio_pin2))
+    {
+        key_up = 0;
+        delay_ms(10);
+        led1_toggle();
+    }
+    return;
+}
+
+#if KEY_TEST
+void key_test(void)
+{
+    push_key1(1);
+
+    return;
+}
+#endif
+
---
+++ src/driver/key/key.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define KEY_TEST 1
+
+void key_init(void);
+void push_key1(uint8_t mode);
+void push_key2(uint8_t mode);
+
+#if KEY_TEST
+void key_test(void);
+#endif
