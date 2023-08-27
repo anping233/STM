0a8614b68a19d39137b4081a81534ffc8d0f13ab
AnPing <2783518354@qq.com>
2023-08-27 13:12:11 +0100

    添加启动程序时的外设初始化配置，timer,spi,pwm

--- src/driver/driver.c
+++ src/driver/driver.c
@@ -2,11 +2,16 @@
 
 
 void driver_init(void) {
+
     led_init();
     led1_init();
+    timer_init();
     usart_init();
     init_24c256();
     oled_init();
     key_init();
+    spi_init();
+    rgb_init();
+
     return;
 }
