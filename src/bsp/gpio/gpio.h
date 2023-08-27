0b053eafda472cfcf0d52c5805d50f185e0537db
AnPing <2783518354@qq.com>
2023-08-27 13:10:19 +0100

    添加外设的引脚信息，usart1,i2c,timer,spi,pwm

--- src/bsp/gpio/gpio.h
+++ src/bsp/gpio/gpio.h
@@ -1,13 +1,37 @@
 #pragma once
 
-#define LED0_GPIO_PORT      (GPIOC)
-#define LED0_GPIO_PIN       (GPIO_Pin_13)
+#define LED0_GPIO_PORT          (GPIOC)
+#define LED0_GPIO_PIN           (gpio_pin13)
 
-#define LED1_GPIO_PORT      (GPIOA)
-#define LED1_GPIO_PIN       (GPIO_Pin_5)
+#define LED1_GPIO_PORT          (GPIOA)
+#define LED1_GPIO_PIN           (gpio_pin5)
 
 #define USART1_GPIO_PORT        (GPIOA)
-#define USART1_GPIO_TXD_PIN     (GPIO_Pin_9)
-#define USART1_GPIO_RXD_PIN     (GPIO_Pin_10)
+#define USART1_GPIO_TXD_PIN     (gpio_pin9)
+#define USART1_GPIO_RXD_PIN     (gpio_pin10)
+
+#define I2C_SCL_GPIO_PORT       (GPIOB)
+#define I2C_SCL_GPIO_PIN        (gpio_pin6)
+#define I2C_SDA_GPIO_PORT       (GPIOB)
+#define I2C_SDA_GPIO_PIN        (gpio_pin7)
+
+#define TIMER_GPIO_PORT         (GPIOA)
+#define TIMER_GPIO_PIN          (gpio_pin0)
+
+#define SPI_CS_GPIO_PORT        (GPIOA)
+#define SPI_CS_GPIO_PIN         (gpio_pin4)
+#define SPI_SCK_GPIO_PORT       (GPIOA)
+#define SPI_SCK_GPIO_PIN        (gpio_pin5)
+#define SPI_MOSI_GPIO_PORT      (GPIOA)
+#define SPI_MOSI_GPIO_PIN       (gpio_pin7)
+#define SPI_MISO_GPIO_PORT      (GPIOA)
+#define SPI_MISO_GPIO_PIN       (gpio_pin6)
+
+#define TIM_PWM_CH1_GPIO_PORT       (GPIOB)
+#define TIM_PWM_CH1_GPIO_PIN       (gpio_pin4)
+#define TIM_PWM_CH2_GPIO_PORT       (GPIOB)
+#define TIM_PWM_CH2_GPIO_PIN       (gpio_pin5)
+#define TIM_PWM_CH3_GPIO_PORT       (GPIOB)
+#define TIM_PWM_CH3_GPIO_PIN       (gpio_pin0)
 
 
