130883b80b11d97bfe9177e9b0ed35a63c96f089
AnPing <2783518354@qq.com>
2023-08-27 12:59:45 +0100

    添加一些字节操作

--- src/libs/libs.h
+++ src/libs/libs.h
@@ -4,10 +4,14 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
+#include <limits.h>
 
 
 #define KHZ     (1000)
 #define MHZ     (KHZ * KHZ)
 
+#define nbits(n) ((uint32_t)((1ull << (n)) - 1))
 
+#define BITS_PER_BYTE   (8)
 
+#define sizeof_array(array)         (sizeof(array) / sizeof(array[0]))
